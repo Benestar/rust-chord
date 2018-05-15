f34a8f6422e6b0c1d5ea166a4db70d83632f8c72
Benedikt Seidl <benedikt.seidl@tum.de>
2018-05-15 10:02:09 +0200

    Use byteorder crate to read message type

--- src/api.rs
+++ src/api.rs
@@ -1,14 +1,30 @@
 use std::error::Error;
+use std::io::Cursor;
 use std::io::prelude::*;
 use std::net::{TcpStream, ToSocketAddrs};
 use std::time::Duration;
 
+use byteorder::{ReadBytesExt, WriteBytesExt, NetworkEndian};
+
+/// The different message types supported by this module
+///
+/// For each message type, there exists a corresponding
+/// struct holding the contents of this message.
 pub enum Message {
+    // TODO
 }
 
 impl Message {
     pub fn new(buffer: &[u8]) -> Result<Message, Box<Error>> {
-        panic!("not implemented")
+        let mut cursor = Cursor::new(&buffer);
+        let size = cursor.read_u16::<NetworkEndian>()? as usize;
+        let msg_type = cursor.read_u16::<NetworkEndian>()?;
+
+        assert_eq!(buffer.len(), size);
+
+        match msg_type {
+            _ => panic!("not implemented")
+        }
     }
 
     pub fn write_bytes(&self, buffer: &mut Vec<u8>) -> Result<Message, Box<Error>> {
@@ -16,14 +32,24 @@ impl Message {
     }
 }
 
+/// Api for vertical communication with another module.
+///
+/// # Examples
+///
+/// ```
+/// let mut api = Api::connect("localhost:8080", 3600);
+///
+/// let msg = api.receive()?;
+/// api.send(&msg);
+/// ```
 pub struct Api {
     stream: TcpStream,
     buffer: Vec<u8>
 }
 
 impl Api {
-    pub fn connect<A: ToSocketAddrs>(addr: A, timeout_ms: u64) -> Result<Api, Box<Error>> {
-        let stream = TcpStream::connect(addr)?;
+    pub fn connect<A: ToSocketAddrs>(addrs: A, timeout_ms: u64) -> Result<Api, Box<Error>> {
+        let stream = TcpStream::connect(addrs)?;
         let buffer = Vec::with_capacity(64000);
 
         let timeout = Duration::from_millis(timeout_ms);
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
+extern crate byteorder;
+
 mod api;
