6ba0ebf1862992f84de2db3e4685413d0a84208a
Stefan Su <stfn.su@live.de>
2018-07-26 14:10:03 +0200

    Implement Api Handler

--- src/handler/api.rs
+++ src/handler/api.rs
@@ -2,37 +2,89 @@ use error::MessageError;
 use message::api::*;
 use message::Message;
 use network::{Connection, ServerHandler};
+use routing::identifier::{Identify, Identifier};
+use routing::Routing;
+use procedures::Procedures;
 use std::error::Error;
 use std::io;
+use std::net::SocketAddr;
+use std::sync::{Mutex, MutexGuard};
+use std::u8;
+use storage::Key;
 
 /// Handler for api requests
 ///
 /// The supported incoming api messages are `DHT GET` and `DHT PUT`.
 pub struct ApiHandler {
-
+    routing: Mutex<Routing<SocketAddr>>,
+    procedures: Procedures
 }
 
 impl ApiHandler {
-    fn handle_dht_get(&self, mut con: Connection, dht_get: DhtGet)
-        -> ::Result<()>
-    {
-        // 1. find peer for id obtained from key
-
-        // 2. send STORAGE GET message to that peer
-
-        // 3. wait for STORAGE GET SUCCESS response
-
-        // 4. send DHT SUCCESS message to api client
-        unimplemented!()
+    /// Creates a new `ApiHandler` instance.
+    pub fn new(routing: Routing<SocketAddr>, timeout: u64) -> Self {
+        Self {
+            routing: Mutex::new(routing),
+            procedures: Procedures::new(timeout)
+        }
     }
 
-    fn handle_dht_put(&self, mut con: Connection, dht_put: DhtPut)
+    /// Acquire the lock
+    fn lock_routing(&self) -> Result<MutexGuard<Routing<SocketAddr>>, &str> {
+        self.routing.lock()
+            .or(Err("Could not lock mutex for routing"))
+    }
+
+    fn get_closest_peer(&self, identifier: Identifier) -> Result<SocketAddr, &str> {
+        let routing = self.lock_routing()?;
+        Ok(**routing.closest_peer(identifier))
+    }
+
+    fn handle_dht_get(&self, mut api_con: Connection, dht_get: DhtGet)
         -> ::Result<()>
     {
-        // 1. find peer for id obtained from key
+        // find peer for id obtained from key
+        let mut replicated_key = Key { raw_key: dht_get.key, replication_index: 0 };
 
-        // 2. send STORAGE PUT message to that peer
-        unimplemented!()
+        // iterate through all replication indices
+        for i in 0..u8::MAX {
+            replicated_key.replication_index = i;
+
+            let identifier = replicated_key.identifier();
+            let closest_peer = self.get_closest_peer(identifier)?;
+            let target_sock_addr =
+                self.procedures.find_peer(identifier, closest_peer)?;
+
+            if let Some(value) = self.procedures.get_value(target_sock_addr, i, dht_get.key)? {
+                let dht_success = DhtSuccess { key: dht_get.key, value };
+                api_con.send(&Message::DhtSuccess(dht_success))?;
+
+                return Ok(())
+            }
+        }
+
+        // send failure if no value was found throughout the iteration
+        let dht_failure = DhtFailure { key: dht_get.key };
+        api_con.send(&Message::DhtFailure(dht_failure))?;
+
+        Ok(())
+    }
+
+    fn handle_dht_put(&self, _con: Connection, dht_put: DhtPut)
+        -> ::Result<()>
+    {
+        let target_replication = dht_put.replication;
+
+        for i in 1..target_replication {
+            let key = Key { raw_key: dht_put.key, replication_index: i };
+            let identifier = key.identifier();
+            let closest_peer = self.get_closest_peer(identifier)?;
+            let target_sock_addr = self.procedures.find_peer(identifier, closest_peer)?;
+
+            self.procedures.put_value(target_sock_addr, i, dht_put.ttl, dht_put.key, dht_put.value.clone())?;
+        }
+
+        Ok(())
     }
 
     fn handle_connection(&self, mut con: Connection) -> ::Result<()> {
--- src/procedures.rs
+++ src/procedures.rs
@@ -2,7 +2,7 @@
 
 use error::MessageError;
 use message::Message;
-use message::p2p::{PeerFind, PredecessorGet, PredecessorSet};
+use message::p2p::{PeerFind, PredecessorGet, PredecessorSet, StorageGet, StoragePut};
 use network::Connection;
 use routing::identifier::Identifier;
 use std::net::SocketAddr;
@@ -41,6 +41,39 @@ impl Procedures {
             }
 
             peer_addr = reply_addr;
+        }
+    }
+
+    pub fn get_value(&self, target_sock_addr: SocketAddr, replication_index: u8, raw_key: [u8; 32]) -> ::Result<Option<Vec<u8>>> {
+        let storage_get = StorageGet { replication_index, raw_key };
+
+        let mut p2p_con = Connection::open(target_sock_addr, 3600)?;
+        p2p_con.send(&Message::StorageGet(storage_get))?;
+
+        let msg = p2p_con.receive()?;
+
+        if let Message::StorageGetSuccess(storage_success) = msg {
+            Ok(Some(storage_success.value))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub fn put_value(&self, target_sock_addr: SocketAddr, replication_index: u8, ttl: u16, raw_key: [u8; 32], value: Vec<u8>) -> ::Result<()> {
+        let storage_put = StoragePut { ttl, replication_index, raw_key, value };
+
+        let mut p2p_con = Connection::open(target_sock_addr, 3600)?;
+        p2p_con.send(&Message::StoragePut(storage_put))?;
+
+        let msg = p2p_con.receive()?;
+
+        match msg {
+            Message::StorageFailure(_) => {
+                eprintln!("key exists in storage already");
+                Ok(())
+            },
+            _ =>
+                Err(Box::new(MessageError::new(msg)))
         }
     }
 
